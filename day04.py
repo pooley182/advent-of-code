2aff5fced4ac0302c0c1621f65e9293b2362c4ac
David Pooley <dave@pooley-mail.co.uk>
2022-12-04 20:52:58 +0000

    Day 4 - part 2

--- day04.py
+++ day04.py
@@ -3,54 +3,37 @@ from utils import fetch
 input_data = fetch(4)
 
 
-def split_range(a: str):
+def split_string_to_range(a: str):
     _range = a.split('-')
     _range[0] = int(_range[0])
     _range[1] = int(_range[1])
-    _range.sort()
-    print(_range)
     return _range
 
 
-def compare(a, b):
+def compare_overlap(a: str, b: str):
     global redundant_elves
-    print('comparing', a, 'and', b)
+    global non_overlaps
 
-    a = split_range(a)
-    elf1 = list(range(a[0], a[1]))
-    elf1.append(a[1])
-    if a[0] == a[1]:
-        elf1.append(a[0])
+    a_range = split_string_to_range(a)
+    elf1 = set(list(range(a_range[0], a_range[1] + 1)))
 
-    b = split_range(b)
-    elf2 = list(range(b[0], b[1]))
-    elf2.append(b[1])
-    if b[0] == b[1]:
-        elf2.append(b[0])
+    b_range = split_string_to_range(b)
+    elf2 = set(list(range(b_range[0], b_range[1] + 1)))
 
-    print('elf1:', elf1)
-    print('elf2:', elf2)
+    if elf1.issubset(elf2) or elf2.issubset(elf1):
+        redundant_elves += 1
 
-    already_counted = False
-
-    if elf1[0] in elf2:
-        if elf1[len(elf1)-1] in elf2:
-            print('elf 1 is inside elf 2')
-            redundant_elves += 1
-            already_counted = True
-    if elf2[0] in elf1:
-        if elf2[len(elf2)-1] in elf1:
-            if already_counted != True:
-                print('elf 2 is inside elf 1')
-                redundant_elves += 1
+    if not elf1.isdisjoint(elf2):
+        non_overlaps += 1
 
 
 redundant_elves = 0
+non_overlaps = 0
 
 for line in input_data:
     line = line.decode("utf-8").strip()
     elf = line.split(',')
-    compare(elf[0], elf[1])
-    print('')
+    compare_overlap(elf[0], elf[1])
 
-print(redundant_elves)  # 526
+print(redundant_elves)
+print(non_overlaps)
