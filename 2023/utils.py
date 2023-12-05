5a88f3af3e6ed7bd01713b1d3498aba587560ef7
David Pooley <dave@pooley-mail.co.uk>
2023-12-05 20:45:17 +0000

    add helper utils

---
+++ 2023/utils.py
@@ -0,0 +1,12 @@
+import datetime
+import urllib.request
+
+TODAY = datetime.datetime.today().day
+
+
+def fetch(day: int = TODAY):
+    request = urllib.request.Request('https://adventofcode.com/2023/day/' + str(day) + '/input')
+    request.add_header("Cookie",
+                       "")
+
+    return urllib.request.urlopen(request)
